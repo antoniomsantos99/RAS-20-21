01f2d745ddf95e3652906d5b7bb94c0f3cfde71c
manuelm00 <49485297+manuelm00@users.noreply.github.com>
2022-01-17 01:13:26 +0000

    ?

--- Parte3/RASBET/src/Controller/RASBet.java
+++ Parte3/RASBET/src/Controller/RASBet.java
@@ -13,18 +13,16 @@ import java.util.Objects;
 
 public class RASBet {
     private Map<String, UtilizadorAutenticado> utilizadores;
-    private Map<Integer, Sessao> sessoes;
 
     public RASBet(){
         this.utilizadores= UtilizadorDAO.getInstance();
     }
 
     public void run() {
-        Sessao atual = new Sessao();
 
     }
 
-    public Boolean login(Sessao s, String email,String password) throws PasswordIncorreta {
+    public Boolean login(String email,String password) throws PasswordIncorreta {
         boolean sucesso = false;
         UtilizadorAutenticado u = this.utilizadores.get(email); //fazer pedido do utilizador
         if(Objects.equals(u.getPassword(), password)){
@@ -32,7 +30,6 @@ public class RASBet {
             u.setLoggedIn(true);
         }
         else throw new PasswordIncorreta();
-        s.setUtilizador(u);
         return sucesso;
     }
 
@@ -40,12 +37,11 @@ public class RASBet {
         return this.utilizadores.containsKey(email);
     }
 
-    public void registarUtilizador(Sessao s, String username,String email,String password){
+    public void registarUtilizador(String username,String email,String password){
         Carteira c = new Carteira();
         ArrayList<Aposta> hist = new ArrayList<>();
         UtilizadorAutenticado u = new UtilizadorAutenticado(false,username,email,password,c,hist);
         this.utilizadores.put(email,u);
-        s.setUtilizador(u);
     }
 
 }
